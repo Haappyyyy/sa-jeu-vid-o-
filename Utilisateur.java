5f44bdcf16e7d52acaac1a4cb5d3a4cd3caae034
djylorias <118692997+djylorias@users.noreply.github.com>
2023-01-04 12:55:52 +0100

    Update Utilisateur.java

--- Utilisateur.java
+++ Utilisateur.java
@@ -1,7 +1,15 @@
 class Utilisateur {
+    /*-------------------------------Infos de base-------------------------------*/
+    int id;
     String prenom;
     String nom;
     String niveau;
-    int vies = 5;
-    int victoires = 0;
+    /*-------------------------------Stats premier jeu-------------------------------*/
+    int premierjeu_vies = 5;
+    int premierjeu_victoires = 0;
+    int premierjeu_parties_gagner;
+    int premierjeu_parties_perdu;
+    int premierjeu_mots_traduits;
+    /*-------------------------------Stats deuxième jeu-------------------------------*/
+    
 }
