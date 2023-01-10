fe81d10b94f96574966ccc1345031b605e6d011c
djylorias <118692997+djylorias@users.noreply.github.com>
2023-01-10 18:54:37 +0100

    Add files via upload

---
+++ ludo/HelloWord!/ressources/partieJCJ.java
@@ -0,0 +1,12 @@
+class partieJCJ{
+    String[] GUI;
+
+    Utilisateur premierJoueur;
+    Utilisateur deuxiemeJoueur;
+
+    int pointsAccorderPremierJoueur = 1;
+    int pointsAccorderDeuxiemeJoueur = 1;
+
+    boolean quitter = false;
+    boolean recommencer = true;
+}
